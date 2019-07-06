dc2d5d4de503feeee6e2cd3b67df43010e090508
RenZhouShan <793813764@qq.com>
2019-07-06 19:18:14 +0800

    model

--- LearnOpenGL/LearnOpenGL/Shader.h
+++ LearnOpenGL/LearnOpenGL/Shader.h
@@ -13,39 +13,33 @@ class Shader
 {
 public:
 	unsigned int ID;
-	// constructor generates the shader on the fly
-	// ------------------------------------------------------------------------
-	Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr)
-	{
-		// 1. retrieve the vertex/fragment source code from filePath
+	Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr) {
 		std::string vertexCode;
 		std::string fragmentCode;
 		std::string geometryCode;
+
 		std::ifstream vShaderFile;
 		std::ifstream fShaderFile;
 		std::ifstream gShaderFile;
-		// ensure ifstream objects can throw exceptions:
+
 		vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 		fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 		gShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-		try
-		{
-			// open files
+		try {
 			vShaderFile.open(vertexPath);
 			fShaderFile.open(fragmentPath);
 			std::stringstream vShaderStream, fShaderStream;
-			// read file's buffer contents into streams
+
 			vShaderStream << vShaderFile.rdbuf();
 			fShaderStream << fShaderFile.rdbuf();
-			// close file handlers
+
 			vShaderFile.close();
 			fShaderFile.close();
-			// convert stream into string
+
 			vertexCode = vShaderStream.str();
 			fragmentCode = fShaderStream.str();
-			// if geometry shader path is present, also load a geometry shader
-			if (geometryPath != nullptr)
-			{
+
+			if (geometryPath != nullptr) {
 				gShaderFile.open(geometryPath);
 				std::stringstream gShaderStream;
 				gShaderStream << gShaderFile.rdbuf();
@@ -53,25 +47,24 @@ public:
 				geometryCode = gShaderStream.str();
 			}
 		}
-		catch (std::ifstream::failure e)
-		{
+		catch (std::ifstream::failure e) {
 			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
 		}
 		const char* vShaderCode = vertexCode.c_str();
-		const char * fShaderCode = fragmentCode.c_str();
-		// 2. compile shaders
+		const char* fShaderCode = fragmentCode.c_str();
+
 		unsigned int vertex, fragment;
-		// vertex shader
+
 		vertex = glCreateShader(GL_VERTEX_SHADER);
 		glShaderSource(vertex, 1, &vShaderCode, NULL);
 		glCompileShader(vertex);
 		checkCompileErrors(vertex, "VERTEX");
-		// fragment Shader
+
 		fragment = glCreateShader(GL_FRAGMENT_SHADER);
 		glShaderSource(fragment, 1, &fShaderCode, NULL);
 		glCompileShader(fragment);
 		checkCompileErrors(fragment, "FRAGMENT");
-		// if geometry shader is given, compile geometry shader
+
 		unsigned int geometry;
 		if (geometryPath != nullptr)
 		{
@@ -81,7 +74,7 @@ public:
 			glCompileShader(geometry);
 			checkCompileErrors(geometry, "GEOMETRY");
 		}
-		// shader Program
+
 		ID = glCreateProgram();
 		glAttachShader(ID, vertex);
 		glAttachShader(ID, fragment);
@@ -89,20 +82,15 @@ public:
 			glAttachShader(ID, geometry);
 		glLinkProgram(ID);
 		checkCompileErrors(ID, "PROGRAM");
-		// delete the shaders as they're linked into our program now and no longer necessery
+
 		glDeleteShader(vertex);
 		glDeleteShader(fragment);
 		if (geometryPath != nullptr)
 			glDeleteShader(geometry);
-
 	}
-	// activate the shader
-	// ------------------------------------------------------------------------
-	void use()
-	{
+	void use() {
 		glUseProgram(ID);
 	}
-	// utility uniform functions
 	// ------------------------------------------------------------------------
 	void setBool(const std::string &name, bool value) const
 	{
@@ -160,10 +148,7 @@ public:
 	{
 		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 	}
-
 private:
-	// utility function for checking shader compilation/linking errors.
-	// ------------------------------------------------------------------------
 	void checkCompileErrors(GLuint shader, std::string type)
 	{
 		GLint success;
--- LearnOpenGL/LearnOpenGL/shader/shader.fs
+++ LearnOpenGL/LearnOpenGL/shader/shader.fs
@@ -1,15 +1,8 @@
 #version 330 core
 out vec4 FragColor;
-
-struct Material {
-    sampler2D diffuse;
-    sampler2D specular;    
-    float shininess;
-}; 
-
-struct Light {
-    vec3 position;
+struct Light{
     vec3 direction;
+    vec3 position;
     float cutOff;
     float outerCutOff;
 
@@ -21,9 +14,13 @@ struct Light {
     float linear;
     float quadratic;
 };
-
-in vec3 FragPos;  
-in vec3 Normal;  
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;    
+    float shininess;
+}; 
+in vec3 FragPos;
+in vec3 Normal;
 in vec2 TexCoords;
   
 uniform vec3 viewPos;
@@ -32,40 +29,26 @@ uniform Light light;
 
 void main()
 {
-    vec3 lightDir = normalize(light.position - FragPos);
-    float theta = dot(lightDir, normalize(-light.direction));
-    float epsilon   = light.cutOff - light.outerCutOff;
-    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
 
-    //if(theta > light.cutOff)
-    //{       
-      // 执行光照计算
-       vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
-  
-        // diffuse 
-        vec3 norm = normalize(Normal);
-        // vec3 lightDir = normalize(light.position - FragPos);
-        float diff = max(dot(norm, lightDir), 0.0);
-        vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
+    vec3 norm = normalize(Normal);
     
-        // specular
-        vec3 viewDir = normalize(viewPos - FragPos);
-        vec3 reflectDir = reflect(-lightDir, norm);  
-        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-        vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
-    
-        float distance    = length(light.position - FragPos);
-        float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+    vec3 lightDir = normalize(-light.direction);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
 
-        //ambient  *= attenuation; 
-        diffuse  *= intensity;
-        specular *= intensity;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
 
-        vec3 result = ambient + diffuse + specular;
-        FragColor = vec4(result, 1.0);
-   // }
-    //else  // 否则，使用环境光，让场景在聚光之外时不至于完全黑暗
-   //     FragColor = vec4(light.ambient * texture(material.diffuse, TexCoords).rgb, 1.0);
-    // ambient
-   
-} 
+    float distance    = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
+
+    ambient  *= attenuation; 
+    diffuse   *= attenuation;
+    specular *= attenuation;
+
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+}
